b2fba393811d0634ed7bc92d94597b3c35c14a6c
Seth Musulin <sethmusulin@gmail.com>
2014-07-02 09:33:48 -0600

    method extraction in Legican class

--- app/models/legiscan_api.rb
+++ app/models/legiscan_api.rb
@@ -11,26 +11,40 @@ class LegiScanApi
 
   def retrieve_bill_info
     if @year ==Time.now.year
-      response = Faraday.get("http://api.legiscan.com/?key=#{@token}&op=getBill&state=#{@state}&bill=#{@bill}")
-      JSON.parse(response.body)
+      state_and_bill_search
     else
-      response = Faraday.get("http://api.legiscan.com/?key=2577f3697e3597ca2363909f55f143c8&op=getSessionList&state=#{@state}")
-      body = JSON.parse(response.body)
-      sesh = body["sessions"].select { |session| session["name"].include?(@year) }
-      response2= Faraday.get("http://api.legiscan.com/?key=#{@token}&op=getMasterList&id=#{sesh[0]["session_id"]}")
-      body2 = JSON.parse(response2.body)
-      bill_id= 0
-      body2["masterlist"].each do |result|
-        result.each do |bill|
-          if bill["number"] == @bill
-            bill_id = bill["bill_id"]
-          end
+      body = session_and_master_list_search
+      find_bill_id(body)
+    end
+  end
+
+
+  private
+  
+  def state_and_bill_search
+    response = Faraday.get("http://api.legiscan.com/?key=#{@token}&op=getBill&state=#{@state}&bill=#{@bill}")
+    JSON.parse(response.body)
+  end
+
+  def session_and_master_list_search
+    response = Faraday.get("http://api.legiscan.com/?key=2577f3697e3597ca2363909f55f143c8&op=getSessionList&state=#{@state}")
+    body = JSON.parse(response.body)
+    sesh = body["sessions"].select { |session| session["name"].include?(@year) }
+    response2= Faraday.get("http://api.legiscan.com/?key=#{@token}&op=getMasterList&id=#{sesh[0]["session_id"]}")
+    JSON.parse(response2.body)
+  end
+
+  def find_bill_id(response)
+    bill_id = 0
+    response["masterlist"].each do |result|
+      result.each do |bill|
+        if bill["number"] == @bill
+          bill_id = bill["bill_id"]
         end
       end
-      response3 = Faraday.get("http://api.legiscan.com/?key=#{@token}&op=getBill&id=#{bill_id}")
-      JSON.parse(response3.body)
     end
-
+    response3 = Faraday.get("http://api.legiscan.com/?key=#{@token}&op=getBill&id=#{bill_id}")
+    JSON.parse(response3.body)
   end
 
 end
